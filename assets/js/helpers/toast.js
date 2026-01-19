ddc4945d0924874a9b355c00d5fb446f05adede5
Favian Medina <fabinnerself@users.noreply.github.com>
2026-01-19 19:07:04 -0400

    upd toast close fix

--- assets/js/helpers/toast.js
+++ assets/js/helpers/toast.js
@@ -67,18 +67,27 @@ toast.classList.add("toast", `toast--${type}`);
 toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
 
 toast.innerHTML = `
-                <div class="toast__icon">
-                    <i class="${icon}"></i>
-                </div>
-                <div class="toast__body">
-                    <h3 class="toast__title">${title}</h3>
-                    <p class="toast__msg">${message}</p>
-                    <p class="toast__a" > <a  href="#contact" class="projects__image1" >  ${getInContactButton}  </p>
-                </div>
-                <div class="toast__close">
-                    <i class="fas fa-times"></i>
-                </div>
+    <div class="toast__icon">
+        <i class="${icon}"></i>
+    </div>
+    <div class="toast__body">
+        <h3 class="toast__title">${title}</h3>
+        <p class="toast__msg">${message}</p>
+        <p class="toast__a"><a href="#contact" class="projects__image1">${getInContactButton}</a></p>
+    </div>
+    <div class="toast__close">
+        <i class="fas fa-times"></i>
+    </div>
             `;
+
+// Handler para el botón cerrar (X)
+toast.onclick = function (e) {
+  if (e.target.closest(".toast__close")) {
+    main.removeChild(toast);
+    clearTimeout(autoRemoveId);
+  }
+};
+
 main.appendChild(toast);
 }
 }
